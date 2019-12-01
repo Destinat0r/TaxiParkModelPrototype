31f42309fece3e815706a75b10685359095ddb1b
Destinat0r <ashavrn@protonmail.com>
2019-12-01 17:26:59 +0200

    added resourceManager, printAllCars(), constructor

--- src/main/java/com/companic/view/View.java
+++ src/main/java/com/companic/view/View.java
@@ -1,14 +1,25 @@
 package com.companic.view;
 
+import com.companic.model.Car;
+import com.companic.util.ResourceManager;
+
+import java.util.List;
+
 public class View {
 
-    public void printWelcome() {
+    private ResourceManager resourceManager;
 
+    public View() {
+        this.resourceManager = ResourceManager.INSTANCE;
     }
-
-
 
     public void print(String string) {
         System.out.println(string);
+    }
+
+    public void printAllCars(List<Car> cars) {
+        for (Car car : cars) {
+            print(car.toString());
+        }
     }
 }
