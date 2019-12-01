f97d5d8fe33aba706884978fc34f23883f808aa6
Destinat0r <ashavrn@protonmail.com>
2019-12-01 15:52:30 +0200

    created properties for messages, created view with print()

---
+++ src/main/java/com/companic/view/View.java
@@ -0,0 +1,14 @@
+package com.companic.view;
+
+public class View {
+
+    public void printWelcome() {
+
+    }
+
+
+
+    public void print(String string) {
+        System.out.println(string);
+    }
+}
