517e9c46ca31eff07df2ac3b65c1469e13d1403c
Destinat0r <ashavrn@protonmail.com>
2019-12-01 17:22:57 +0200

    added ResourceManager and UTF8Control to util package

---
+++ src/main/java/com/companic/util/ResourceManager.java
@@ -0,0 +1,23 @@
+package com.companic.util;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public enum ResourceManager {
+    INSTANCE;
+
+    private ResourceBundle resourceBundle;
+    private final String resourceName = "messages";
+
+    private ResourceManager() {
+        resourceBundle = ResourceBundle.getBundle(resourceName, new Locale("en"), new UTF8Control());
+    }
+
+    public void changeResource(Locale locale) {
+        resourceBundle = ResourceBundle.getBundle(resourceName, locale, new UTF8Control());
+    }
+
+    public String getString(String key) {
+        return resourceBundle.getString(key);
+    }
+}
