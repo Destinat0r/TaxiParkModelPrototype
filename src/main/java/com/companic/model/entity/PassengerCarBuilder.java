e28a6b8c0400317f92f5821abc32b2137d0c1ee9
Destinat0r <ashavrn@protonmail.com>
2019-12-03 18:22:31 +0200

    added PassengerCarBuilder

---
+++ src/main/java/com/companic/model/entity/PassengerCarBuilder.java
@@ -0,0 +1,43 @@
+package com.companic.model.entity;
+
+import com.companic.model.Body;
+import com.companic.model.CarBuilder;
+
+public class PassengerCarBuilder extends CarBuilder {
+    private Body body;
+    private int passengersAmount;
+
+    public static PassengerCarBuilder passengerCar() {
+        return new PassengerCarBuilder();
+    }
+
+    public PassengerCarBuilder withBody(Body body) {
+        this.body = body;
+        return this;
+    }
+
+    public PassengerCarBuilder withPassengersAmount(int amount) {
+        this.passengersAmount = amount;
+        return this;
+    }
+
+    public PassengerCar build() {
+        return new PassengerCar(this);
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public int getPassengersAmount() {
+        return passengersAmount;
+    }
+
+    public void setPassengersAmount(int passengersAmount) {
+        this.passengersAmount = passengersAmount;
+    }
+}
