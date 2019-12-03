5280df8768a3a456b855dafb0bea1eb5445f2076
Destinat0r <ashavrn@protonmail.com>
2019-12-03 18:24:23 +0200

    added body, passengersAmount, their init in constructor, setter-getter, checkFields()

--- src/main/java/com/companic/model/entity/PassengerCar.java
+++ src/main/java/com/companic/model/entity/PassengerCar.java
@@ -1,11 +1,41 @@
 package com.companic.model.entity;
 
-import com.companic.model.CarBuilder;
+import com.companic.model.Body;
 
 public class PassengerCar extends Car {
 
-    public PassengerCar(CarBuilder builder) {
-        super(builder);
+    private Body body;
+    private int passengersAmount;
 
+    public PassengerCar(PassengerCarBuilder builder) {
+        super(builder);
+        checkFields(builder);
+        this.body = builder.getBody();
+        this.passengersAmount = builder.getPassengersAmount();
+    }
+
+    private void checkFields(PassengerCarBuilder builder) {
+        if (builder.getBody() == null) {
+            throw new IllegalArgumentException("body is not set");
+        }
+        if (builder.getPassengersAmount() <= 0) {
+            throw new IllegalArgumentException("passengersAmount is not set or invalid");
+        }
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public int getPassengersAmount() {
+        return passengersAmount;
+    }
+
+    public void setPassengersAmount(int passengersAmount) {
+        this.passengersAmount = passengersAmount;
     }
 }
