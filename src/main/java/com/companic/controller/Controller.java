395e11a302cbab105ed279b910e8f8e987facc20
Destinat0r <ashavrn@protonmail.com>
2019-12-04 05:13:16 +0200

    Car substituted to PassengerCar, imports

--- src/main/java/com/companic/controller/Controller.java
+++ src/main/java/com/companic/controller/Controller.java
@@ -1,15 +1,17 @@
 package com.companic.controller;
 
 import com.companic.model.Color;
-import com.companic.model.Type;
+import com.companic.model.Body;
 import com.companic.model.entity.Car;
+import com.companic.model.entity.PassengerCar;
 import com.companic.model.entity.TaxiPark;
+import com.companic.model.entity.Truck;
 import com.companic.util.ResourceManager;
 import com.companic.view.View;
 
 import java.util.Locale;
 
-import static com.companic.model.CarBuilder.car;
+import static com.companic.model.entity.PassengerCarBuilder.passengerCar;
 
 public class Controller {
 
@@ -22,35 +24,35 @@ public class Controller {
     public void run() {
         ResourceManager.INSTANCE.changeResource(new Locale("en"));
 
-        Car car1 = car().withVendor("Toyota").withModel("Auris").withYear(2008).withType(Type.HATCHBACK)
-                           .withColor(Color.GREEN).withMaxSpeed(250).withFuelConsumption(4).withValue(2000).build();
+        PassengerCar car1 = passengerCar().withVendor("Toyota").withModel("Auris").withYear(2008).withBody(Body.HATCHBACK)
+                           .withColor(Color.GREEN).withMaxSpeed(250).withFuelConsumption(4).withValue(2000).withPassengersAmount(4).build();
 
-        Car car2 = car().withVendor("Mercedes").withModel("E Class").withYear(2003).withType(Type.SEDAN)
-                           .withColor(Color.GREY).withMaxSpeed(240).withFuelConsumption(7).withValue(3800).build();
+        PassengerCar car2 = passengerCar().withVendor("Mercedes").withModel("E Class").withYear(2003).withBody(Body.SEDAN)
+                           .withColor(Color.GREY).withMaxSpeed(240).withFuelConsumption(7).withValue(3800).withPassengersAmount(4).build();
 
-        Car car3 = car().withVendor("Honda").withModel("Fit").withYear(2004).withType(Type.HATCHBACK)
-                           .withColor(Color.YELLOW).withMaxSpeed(200).withFuelConsumption(3).withValue(4000).build();
+        PassengerCar car3 = passengerCar().withVendor("Honda").withModel("Fit").withYear(2004).withBody(Body.HATCHBACK)
+                           .withColor(Color.YELLOW).withMaxSpeed(200).withFuelConsumption(3).withValue(4000).withPassengersAmount(4).build();
 
-        Car car4 = car().withVendor("Subaru").withModel("Impreza").withYear(2005).withType(Type.SEDAN)
-                           .withColor(Color.GREEN).withMaxSpeed(190).withFuelConsumption(9).withValue(3000).build();
+        PassengerCar car4 = passengerCar().withVendor("Subaru").withModel("Impreza").withYear(2005).withBody(Body.SEDAN)
+                           .withColor(Color.GREEN).withMaxSpeed(190).withFuelConsumption(9).withValue(3000).withPassengersAmount(4).build();
 
-        Car car5 = car().withVendor("Nissan").withModel("Lafesta").withYear(2008).withType(Type.MINI_VAN)
-                           .withColor(Color.GREEN).withMaxSpeed(300).withFuelConsumption(2).withValue(5500).build();
+        PassengerCar car5 = passengerCar().withVendor("Nissan").withModel("Lafesta").withYear(2008).withBody(Body.MINI_VAN)
+                           .withColor(Color.GREEN).withMaxSpeed(300).withFuelConsumption(2).withValue(5500).withPassengersAmount(4).build();
 
-        Car car6 = car().withVendor("Suzuki").withModel("Splash").withYear(2010).withType(Type.MINI_VAN)
-                           .withColor(Color.GREEN).withMaxSpeed(300).withFuelConsumption(2).withValue(5500).build();
+        PassengerCar car6 = passengerCar().withVendor("Suzuki").withModel("Splash").withYear(2010).withBody(Body.MINI_VAN)
+                           .withColor(Color.GREEN).withMaxSpeed(300).withFuelConsumption(2).withValue(5500).withPassengersAmount(4).build();
 
-        Car car7 = car().withVendor("CHEVROLET").withModel("MW WAGON").withYear(2009).withType(Type.WAGON)
-                           .withColor(Color.GREEN).withMaxSpeed(220).withFuelConsumption(2).withValue(5500).build();
+        PassengerCar car7 = passengerCar().withVendor("CHEVROLET").withModel("MW WAGON").withYear(2009).withBody(Body.WAGON)
+                           .withColor(Color.GREEN).withMaxSpeed(220).withFuelConsumption(2).withValue(5500).withPassengersAmount(4).build();
 
-        Car car8 = car().withVendor("Mazda").withModel("RX8").withYear(2006).withType(Type.COUPE)
-                           .withColor(Color.RED).withMaxSpeed(270).withFuelConsumption(8).withValue(7500).build();
+        PassengerCar car8 = passengerCar().withVendor("Mazda").withModel("RX8").withYear(2006).withBody(Body.COUPE)
+                           .withColor(Color.RED).withMaxSpeed(270).withFuelConsumption(8).withValue(7500).withPassengersAmount(4).build();
 
-        Car car9 = car().withVendor("BMW").withModel("6 SERIES").withYear(2014).withType(Type.COUPE)
-                           .withColor(Color.ORANGE).withMaxSpeed(350).withFuelConsumption(10).withValue(30500).build();
+        PassengerCar car9 = passengerCar().withVendor("BMW").withModel("6 SERIES").withYear(2014).withBody(Body.COUPE)
+                           .withColor(Color.ORANGE).withMaxSpeed(350).withFuelConsumption(10).withPassengersAmount(4).withValue(30500).build();
 
-        Car car10 = car().withVendor("Mitsubishi").withModel("Colt Plus").withYear(2006).withType(Type.HATCHBACK)
-                            .withColor(Color.VIOLET).withMaxSpeed(265).withFuelConsumption(6).withValue(1900).build();
+        PassengerCar car10 = passengerCar().withVendor("Mitsubishi").withModel("Colt Plus").withYear(2006).withBody(Body.HATCHBACK)
+                            .withColor(Color.VIOLET).withMaxSpeed(265).withFuelConsumption(6).withPassengersAmount(4).withValue(1900).build();
 
 
         TaxiPark taxiPark = new TaxiPark(car1, car2, car3, car4, car5, car6, car7, car8, car9, car10);
