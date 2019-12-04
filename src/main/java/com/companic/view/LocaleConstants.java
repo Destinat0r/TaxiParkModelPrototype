732da1fcdd53cc382ca1bc667a7c0ac9e3d548f3
Destinat0r <ashavrn@protonmail.com>
2019-12-04 12:12:10 +0200

    added divider and key_value_divider constants

--- src/main/java/com/companic/view/LocaleConstants.java
+++ src/main/java/com/companic/view/LocaleConstants.java
@@ -3,6 +3,8 @@ package com.companic.view;
 public interface LocaleConstants {
     String WELCOME = "welcome";
     String VIEW_ALL = "view_all";
+    String DIVIDER = "divider";
+    String KEY_VALUE_DIVIDER = "key_value_divider";
     String SORT_BY_FUEL_CONSUMPTION_ASC = "sort_by_fuel_consumption_asc";
     String TOTAL_VALUE = "total_value";
     String WITHIN_MAX_SPEED_RANGE = "within_max_speed_range";
