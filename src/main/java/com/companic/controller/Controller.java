a2f770cfd38f76d07189e557ec3b2cc48a8dcfe0
Destinat0r <ashavrn@protonmail.com>
2019-12-04 11:54:31 +0200

    added lisence plated to cars instantiation

--- src/main/java/com/companic/controller/Controller.java
+++ src/main/java/com/companic/controller/Controller.java
@@ -22,40 +22,40 @@ public class Controller {
     public void run() {
         ResourceManager.INSTANCE.changeResource(new Locale("en"));
 
-        PassengerCar car1 = passengerCar().withVendor("Toyota").withModel("Auris").withYear(2008).withBody(Body.HATCHBACK)
+        PassengerCar car1 = passengerCar().withLicense("12GH1G5").withVendor("Toyota").withModel("Auris").withYear(2008).withBody(Body.HATCHBACK)
                            .withColor(Color.GREEN).withMaxSpeed(250).withFuelConsumption(4).withValue(2000).withPassengersAmount(4).build();
 
-        PassengerCar car2 = passengerCar().withVendor("Mercedes").withModel("E Class").withYear(2003).withBody(Body.SEDAN)
+        PassengerCar car2 = passengerCar().withLicense("G40B0SQ").withVendor("Mercedes").withModel("E Class").withYear(2003).withBody(Body.SEDAN)
                            .withColor(Color.GREY).withMaxSpeed(240).withFuelConsumption(7).withValue(3800).withPassengersAmount(4).build();
 
-        PassengerCar car3 = passengerCar().withVendor("Honda").withModel("Fit").withYear(2004).withBody(Body.HATCHBACK)
+        PassengerCar car3 = passengerCar().withLicense("7FOIJ4M").withVendor("Honda").withModel("Fit").withYear(2004).withBody(Body.HATCHBACK)
                            .withColor(Color.YELLOW).withMaxSpeed(200).withFuelConsumption(3).withValue(4000).withPassengersAmount(4).build();
 
-        PassengerCar car4 = passengerCar().withVendor("Subaru").withModel("Impreza").withYear(2005).withBody(Body.SEDAN)
+        PassengerCar car4 = passengerCar().withLicense("J45V9MM").withVendor("Subaru").withModel("Impreza").withYear(2005).withBody(Body.SEDAN)
                            .withColor(Color.GREEN).withMaxSpeed(190).withFuelConsumption(9).withValue(3000).withPassengersAmount(4).build();
 
-        PassengerCar car5 = passengerCar().withVendor("Nissan").withModel("Lafesta").withYear(2008).withBody(Body.MINI_VAN)
+        PassengerCar car5 = passengerCar().withLicense("8N23KD0").withVendor("Nissan").withModel("Lafesta").withYear(2008).withBody(Body.MINI_VAN)
                            .withColor(Color.GREEN).withMaxSpeed(300).withFuelConsumption(2).withValue(5500).withPassengersAmount(4).build();
 
-        PassengerCar car6 = passengerCar().withVendor("Suzuki").withModel("Splash").withYear(2010).withBody(Body.MINI_VAN)
+        PassengerCar car6 = passengerCar().withLicense("3FJNSV9").withVendor("Suzuki").withModel("Splash").withYear(2010).withBody(Body.MINI_VAN)
                            .withColor(Color.GREEN).withMaxSpeed(300).withFuelConsumption(2).withValue(5500).withPassengersAmount(4).build();
 
-        PassengerCar car7 = passengerCar().withVendor("CHEVROLET").withModel("MW WAGON").withYear(2009).withBody(Body.WAGON)
+        PassengerCar car7 = passengerCar().withLicense("8GML1F4").withVendor("CHEVROLET").withModel("MW WAGON").withYear(2009).withBody(Body.WAGON)
                            .withColor(Color.GREEN).withMaxSpeed(220).withFuelConsumption(2).withValue(5500).withPassengersAmount(4).build();
 
-        PassengerCar car8 = passengerCar().withVendor("Mazda").withModel("RX8").withYear(2006).withBody(Body.COUPE)
+        PassengerCar car8 = passengerCar().withLicense("KN39FH3").withVendor("Mazda").withModel("RX8").withYear(2006).withBody(Body.COUPE)
                            .withColor(Color.RED).withMaxSpeed(270).withFuelConsumption(8).withValue(7500).withPassengersAmount(4).build();
 
-        PassengerCar car9 = passengerCar().withVendor("BMW").withModel("6 SERIES").withYear(2014).withBody(Body.COUPE)
+        PassengerCar car9 = passengerCar().withLicense("9GHEVD0").withVendor("BMW").withModel("6 SERIES").withYear(2014).withBody(Body.COUPE)
                            .withColor(Color.ORANGE).withMaxSpeed(350).withFuelConsumption(10).withPassengersAmount(4).withValue(30500).build();
 
-        PassengerCar car10 = passengerCar().withVendor("Mitsubishi").withModel("Colt Plus").withYear(2006).withBody(Body.HATCHBACK)
+        PassengerCar car10 = passengerCar().withLicense("23RFS1X").withVendor("Mitsubishi").withModel("Colt Plus").withYear(2006).withBody(Body.HATCHBACK)
                             .withColor(Color.VIOLET).withMaxSpeed(265).withFuelConsumption(6).withPassengersAmount(4).withValue(1900).build();
 
-        Truck car11 = truck().withVendor("Nissan").withModel("Lafesta").withYear(2008).withDuty(Duty.LIGHT).withPayload(2000)
+        Truck car11 = truck().withVendor("Nissan").withLicense("9FH4SL9").withModel("Lafesta").withYear(2008).withDuty(Duty.LIGHT).withPayload(2000)
                        .withColor(Color.GREEN).withMaxSpeed(150).withFuelConsumption(10).withValue(7500).build();
 
-        Truck car12 = truck().withVendor("Nissan").withModel("Lafesta").withYear(2008).withDuty(Duty.MEDIUM).withPayload(5000)
+        Truck car12 = truck().withVendor("Nissan").withLicense("4RSL95H").withModel("Lafesta").withYear(2008).withDuty(Duty.MEDIUM).withPayload(5000)
                        .withColor(Color.BLUE).withMaxSpeed(140).withFuelConsumption(15).withValue(17500).build();;
 
 
