b5abb5801f7406cd5ac63d8bb8da49a1dd2fc2c0
Destinat0r <ashavrn@protonmail.com>
2019-12-06 07:02:13 +0200

    added current locale property and constant

--- src/main/java/com/companic/utils/ConfigConstants.java
+++ src/main/java/com/companic/utils/ConfigConstants.java
@@ -4,4 +4,5 @@ public interface ConfigConstants {
 
     String PATH_JSON_PASSENGER_CARS = "file.json.passenger_cars";
     String PATH_JSON_TRUCKS = "file.json.trucks";
+    String LOCALE_CURRENT = "locale.current";
 }
