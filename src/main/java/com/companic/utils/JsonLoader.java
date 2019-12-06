7a9f1e9bde96394253c9d8e20b9cf34fd19d0367
Destinat0r <ashavrn@protonmail.com>
2019-12-06 11:46:52 +0200

    added JavaDoc

--- src/main/java/com/companic/utils/JsonLoader.java
+++ src/main/java/com/companic/utils/JsonLoader.java
@@ -14,6 +14,11 @@ import java.io.Reader;
 public class JsonLoader {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
+    /**
+     *Writes array of objects to Json
+     * @param objects
+     * @param filePath
+     */
     public static void writeToJson(Object[] objects, String filePath) {
         try (FileWriter writer = new FileWriter(filePath)) {
             GSON.toJson(objects, writer);
@@ -22,6 +27,12 @@ public class JsonLoader {
         }
     }
 
+    /**
+     * Read objects from Json to array
+     * @param filePath
+     * @param clazz class of object to read. Has to be in form YourObject[].class
+     * @return array of objects of pointed type
+     */
     public static <T> T[] readFromJson(String filePath, Class<T[]> clazz) {
         T[] objects = null;
         try (Reader reader = new FileReader(filePath)) {
