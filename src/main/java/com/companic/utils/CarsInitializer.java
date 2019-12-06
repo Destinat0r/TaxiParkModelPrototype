118e497eed6d505503c0fb7b346b36ec3326105d
Destinat0r <ashavrn@protonmail.com>
2019-12-06 06:54:26 +0200

    added CarsInitializer - loads cars from json

---
+++ src/main/java/com/companic/utils/CarsInitializer.java
@@ -0,0 +1,17 @@
+package com.companic.utils;
+
+import com.companic.model.entity.PassengerCar;
+import com.companic.model.entity.Truck;
+
+public class CarsInitializer {
+
+    private static final ResourceManager RESOURCE_MANAGER = ResourceManager.INSTANCE;
+
+    public static PassengerCar[] initPassengerCarsFromJsonFile() {
+        return JsonLoader.loadFromJson(RESOURCE_MANAGER.getParameter(ConfigConstants.PATH_JSON_PASSENGER_CARS), PassengerCar[].class);
+    }
+
+    public static Truck[] initTrucksFromJsonFile() {
+        return JsonLoader.loadFromJson(RESOURCE_MANAGER.getParameter(ConfigConstants.PATH_JSON_TRUCKS), Truck[].class);
+    }
+}
