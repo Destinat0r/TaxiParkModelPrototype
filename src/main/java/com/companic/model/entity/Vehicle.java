54b1f12843dec7dd31a03c078c49763aebd23bcb
Destinat0r <ashavrn@protonmail.com>
2019-12-14 22:20:02 +0200

    added methods to Vihecle

--- src/main/java/com/companic/model/entity/Vehicle.java
+++ src/main/java/com/companic/model/entity/Vehicle.java
@@ -1,5 +1,36 @@
 package com.companic.model.entity;
 
 public interface Vehicle {
+    int getFuelConsumption();
+
+    String getLicensePlate();
+
+    void setLicensePlate(String licensePlate);
+
+    String getVendor();
+
+    void setVendor(String vendor);
+
+    String getModel();
+
+    void setModel(String model);
+
+    int getYear();
+
+    void setYear(int year);
+
+    Color getColor();
+
+    void setColor(Color color);
+
+    int getMaxSpeed();
+
+    void setMaxSpeed(int maxSpeed);
+
+    void setFuelConsumption(int fuelConsumption);
+
+    int getValue();
+
+    void setValue(int value);
 
 }
